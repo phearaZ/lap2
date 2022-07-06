d3e782cf32b2043338fd7d0ed856e217440c3a8a
PhearaZ <sophearachhorn03@gmail.com>
2022-07-06 16:35:06 +0700

    Add project files.

---
+++ lap2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabExercise2
+{
+    class Movie
+    {
+        public string strTitle;
+        public string strGenre;
+        public int intYear;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie objMovie1;            //part (b) declare an object name of type movie
+            objMovie1 = new Movie();    //part (c) instantiate the object objMovie1
+            Console.Write("Title: ");
+            objMovie1.strTitle = Console.ReadLine();
+            Console.Write("Genre: ");
+            objMovie1.strGenre = Console.ReadLine();
+            Console.Write("year: ");
+            objMovie1.intYear = int.Parse(Console.ReadLine());
+            Console.WriteLine("Movie is {0}, Genre is {1}, year at {2}", objMovie1.strTitle, objMovie1.strGenre, objMovie1.intYear);
+        }
+    }
+}
---
+++ lesson01exercise3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson01exercise3
+{
+    class MedicalRecord
+    {
+        public string strPatientName;
+        public string strDoctorName;
+        public float fltFees;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MedicalRecord objM1;
+            objM1= new MedicalRecord();
+            Console.Write("patient name: ");
+            objM1.strPatientName = Console.ReadLine();
+            Console.Write("doctor name: ");
+            objM1.strDoctorName = Console.ReadLine();
+            Console.Write("fees: ");
+            objM1.fltFees = float.Parse(Console.ReadLine());
+
+            MedicalRecord objM2;
+            objM2= new MedicalRecord();
+            Console.Write("patient name 2: ");
+            objM2.strPatientName = Console.ReadLine();
+            Console.Write("doctor name 2: ");
+            objM2.strDoctorName = Console.ReadLine();
+            Console.Write("fees: ");
+            objM2.fltFees = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Patient Name is {0}, Doctor Name is {1}, Fees: {2}, Patient Name 2 is {3}, Doctor Name 2 is {4}, Fees: {5}", objM1.strPatientName, objM1.strDoctorName, objM1.fltFees, objM2.strPatientName, objM2.strDoctorName, objM2.fltFees);
+
+        }
+    }
+}
